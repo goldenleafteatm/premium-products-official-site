ab667d12e7ed4e4445c23ce8e7c3c73a8b4f7064
goldenleafteatm <goldenleafteatm@gmail.com>
2026-06-05 12:47:49 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,50 @@
+const images = [
+
+"assets/images/products/origin-black/main.jpg",
+
+"assets/images/products/origin-black/gallery1.jpg",
+
+"assets/images/products/origin-black/gallery2.jpg",
+
+"assets/images/products/origin-black/gallery3.jpg"
+
+];
+
+let currentImage = 0;
+
 function changeImage(image){
 
 document.getElementById("mainImage").src =
 image.src;
+
+}
+
+function nextImage(){
+
+currentImage++;
+
+if(currentImage >= images.length){
+
+currentImage = 0;
+
+}
+
+document.getElementById("mainImage").src =
+images[currentImage];
+
+}
+
+function previousImage(){
+
+currentImage--;
+
+if(currentImage < 0){
+
+currentImage = images.length - 1;
+
+}
+
+document.getElementById("mainImage").src =
+images[currentImage];
 
 }
